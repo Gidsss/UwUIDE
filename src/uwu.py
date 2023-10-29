b20cdd77dad118f4023b80b18a8959929339da82
chaaals <charlesching0808@gmail.com>
2023-10-29 15:05:16 +0800

    chore: add frames and created an instance of CTk class

--- src/uwu.py
+++ src/uwu.py
@@ -1,3 +1,27 @@
-# This is where we'll implement GUI
+import customtkinter as ctk
+
+class UwUCodeEditor(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        # TODO: Add widgets for respective frames
+        pass
+
+class UwUConsole(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        # TODO: Add widgets for respective frames
+        pass
+
+class UwULexer(ctk.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        # TODO: Add widgets for respective frames
+        pass
+
+class UwU(ctk.CTk):
+    def __init__(self):
+        # TODO: Init window settings and display arrange different frames
+        pass
+
 if __name__ == "__main__":
     print('Lexer GUI')
