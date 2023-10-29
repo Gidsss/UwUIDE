30ff5e2495e8ff7c830c5f62521fab7006eefdb5
chaaals <charlesching0808@gmail.com>
2023-10-29 16:32:11 +0800

    chore: handle case when OS is macOS

--- tasks.py
+++ tasks.py
@@ -1,15 +1,23 @@
+import platform
 from invoke import task
+
+SYSTEM = platform.system()
+COMMAND = "python"
+
+if SYSTEM == 'Darwin': # for macOS
+    COMMAND = 'python3'
 
 @task
 def test(c, file = None):
     print('Running Tests...')
     if file is not None:
         print(file)
-        c.run(f"python -m pytest test/{file}")
+        c.run(f"{COMMAND} -m pytest test/{file}")
     else:
-        c.run('python -m pytest test/')
+        c.run(f"{COMMAND} -m pytest test/")
 
 @task
 def build(c):
     print('Building UwU++ IDE...')
-    c.run('python -m src.uwu')
+    
+    c.run(f"{COMMAND} -m src.uwu")
