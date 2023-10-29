2450c36ce943ec02f824965177ce2a7878479d9b
chaaals <charlesching0808@gmail.com>
2023-10-29 22:59:45 +0800

    chore: added documentation per fn

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ This script serves a package manager that mainly checks dependencies and run sub
 
 To use this, simply run this command in the command line:
 
-    python -m run < subprocess_name > < optional params >
+    python -m run < install | uninstall | build | test > < optional params >
 """
 
 import sys
 import subprocess
 import importlib
+from pathlib import Path
 
 class PackageManager():
     def run_subprocess(self, commands: list[str], script) -> None:
@@ -25,17 +26,56 @@ class PackageManager():
         except ImportError:
             raise ImportError(f"{package_name} is not installed!")
 
-# Instal dependencies
-def install(pm: PackageManager) -> None:
+# Install dependencies
+def install(pm: PackageManager, package_name = None) -> None:
+    """
+    This function can install packages from requirements.txt or one by one.
+
+    python -m run install
+    python -m run install < package name >
+    """
     pm.check_dependency('pip')
-    pm.run_subprocess(['pip', 'install', '-r'], 'requirements.txt')
+    commands = ['pip', 'install']
+
+    if package_name is not None:
+        pm.run_subprocess(commands, package_name)
+    else:
+        pm.run_subprocess([*commands, '-r'], 'requirements.txt')
+
+# Uninstall dependencies
+def uninstall(pm: PackageManager, package_name = None) -> None:
+    """
+    This function can uninstall packages from requirements.txt or one by one.
+
+    python -m run uninstall
+    python -m run uninstall < package name >
+    """
+    pm.check_dependency('pip')
+    commands = ['pip', 'uninstall']
+
+    if package_name is not None:
+        pm.run_subprocess(commands, package_name)
+    else:
+        pm.run_subprocess([*commands, '-r'], 'requirements.txt')
 
 # Run IDE
-def build(pm: PackageManager) -> None:        
+def build(pm: PackageManager) -> None:      
+    """
+    This function can build the UwU IDE.
+
+    python -m run build
+    """  
     pm.run_subprocess([sys.executable, '-m'], 'src.uwu')
 
 # Run pytest
 def test(pm: PackageManager, filename = None) -> None:
+    """
+    This function can run pytest.
+
+    python -m run test
+    python -m run test test_*
+    python -m run test *_test
+    """
     pm.check_dependency('pytest')
 
     script = 'test/'
@@ -53,6 +93,6 @@ if __name__ == '__main__':
         raise TypeError("Please specify which script to run. (install | build | test)")
     
     try:
-        globals()[args[0]](pm=pm, *args[1:])
+        globals()[args[0]](pm, *args[1:])
     except KeyError:
         raise KeyError(f"Cannot call function {args[0]}. It should be of type (install | build | test)")
