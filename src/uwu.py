186fad65f3f795fa4e10c84d0303b4d91b7d501c
chaaals <charlesching0808@gmail.com>
2023-11-02 23:05:50 +0800

    feat: implemented analyzer tabs and refactored analyzer panel

--- src/uwu.py
+++ src/uwu.py
@@ -2,6 +2,7 @@ from customtkinter import *
 from .components.lexer_table import LexerTable
 from .components.codeview import CodeView
 from .components.command_menu import CommandMenu
+from .components.analyzer_tabs import UwULexerTab, UwUParserTab
 
 class UwUCodePanel(CTkFrame):
     def __init__(self, master, **kwargs):
@@ -20,27 +21,24 @@ class UwUCodePanel(CTkFrame):
         console = CTkLabel(master=self, text='console', bg_color='#1A1B26', text_color='#FFFFFF')
         console.grid(row=3, rowspan=2, columnspan=4, stick='nsew', padx=12, pady=12)
 
-class UwULexerPanel(CTkScrollableFrame):
+class UwuAnalyzerPanel(CTkTabview):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
 
-        self.grid_columnconfigure((0,1), weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=10)
-        self.grid_rowconfigure(2, weight=10)
+        self.lexer_tab = self.add('Lexer')
+        self.parser_tab = self.add('Parser')
 
-        self.lexeme_label = CTkLabel(master=self, text='Lexeme', bg_color='#333652', text_color='#FFFFFF')
-        self.token_label = CTkLabel(master=self, text='Token', bg_color='#333652', text_color='#FFFFFF')
+        self.lexer_tab.grid_columnconfigure((0,1), weight=1)
+        self.lexer_tab.grid_columnconfigure((0,1), weight=1)
 
-        self.lexeme_label.grid(row=0, column=0, sticky='nsew')
-        self.token_label.grid(row=0, column=1, sticky='nsew')
+        self.parser_tab.grid_columnconfigure((0,1), weight=1)
+        self.parser_tab.grid_columnconfigure((0,1), weight=1)
 
-        self.lexer_table = LexerTable(master=self, fg_color='transparent', data=[
-            ('fwunc', 'FUNCTION'),
-            ('berry', 'IDENTIFIER'),
-            ('fwunc', 'FUNCTION'),
-        ])
-        self.lexer_table.grid(row=1, rowspan=2 ,columnspan=2, sticky='nsew')
+        self.lexer_tab_content = UwULexerTab(self.lexer_tab, fg_color='transparent')
+        self.lexer_tab_content.grid(row=0, column=0, rowspan=2, columnspan=2, sticky='nsew')
+
+        self.parser_tab_content = UwUParserTab(self.parser_tab, fg_color='transparent')
+        self.parser_tab_content.grid(row=0, column=0, rowspan=2, columnspan=2, sticky='nsew')
 
 class UwU(CTk):
     def __init__(self):
@@ -58,8 +56,17 @@ class UwU(CTk):
         code_panel = UwUCodePanel(master=self, fg_color='transparent')
         code_panel.grid(row=0, column=0, rowspan=5, columnspan=4, sticky='nsew')
 
-        lexer_panel = UwULexerPanel(master=self, fg_color='#1A1B26', corner_radius=0, label_text='Lexer', label_fg_color='#1A1B26', label_text_color='#FFFFFF')
-        lexer_panel.grid(row=0, column=4, rowspan=5, columnspan=2, sticky='nsew')
+        analyzer_panel = UwuAnalyzerPanel(
+            master=self,
+            fg_color='#1A1B26',
+            corner_radius=20,
+            anchor='nw',
+            segmented_button_fg_color='#1A1B26',
+            segmented_button_selected_color='#333652',
+            segmented_button_selected_hover_color='gray',
+            segmented_button_unselected_color='#1A1B26',segmented_button_unselected_hover_color='gray'
+        )
+        analyzer_panel.grid(row=0, column=4, rowspan=5, columnspan=2, sticky='nsew')
 
 if __name__ == "__main__":
     app = UwU()
