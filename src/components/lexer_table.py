af2c05541eeecb4e7321c3e532206330fffddbe3
HolyShaq <acebyronehalili@gmail.com>
2023-11-05 17:18:13 +0800

    Token Highlighting

--- src/components/lexer_table.py
+++ src/components/lexer_table.py
@@ -1,32 +1,50 @@
 from customtkinter import *
 
 from src.lexer import Token
+from src.components.code_view import Remote, Tags
+
 
 class LexerTable(CTkFrame):
     def __init__(self, master, tokens: list[Token], **kwargs):
         super().__init__(master, **kwargs)
         self.tokens = tokens
         self.columns = (0, 1)
-        self.rows = tuple([i for i in range(len(self.tokens))]) if len(self.tokens) > 0 else (0,1,2,3,4)
+        self.rows = tuple([i for i in range(len(self.tokens))]) if len(self.tokens) > 0 else (0, 1, 2, 3, 4)
 
         self.grid_columnconfigure(self.columns, weight=1)
         self.grid_rowconfigure(self.rows, weight=1)
 
-        for i in range(len(self.tokens)):
-            self.lexeme = CTkLabel(master=self, text=self.tokens[i].lexeme, fg_color='transparent', text_color='#FFFFFF')
+        code_editor = Remote.code_editor_instance
+        self.lexemes_labels = []
+        self.token_labels = []
+        for i, token in enumerate(self.tokens):
+            lexeme_label = CTkLabel(master=self, text=token.lexeme, fg_color='transparent', text_color='#FFFFFF')
+            token_label = CTkLabel(master=self, text=token.token, fg_color='transparent', text_color='#FFFFFF')
 
-            self.token = CTkLabel(master=self, text=self.tokens[i].token, fg_color='transparent', text_color='#FFFFFF')
+            lexeme_label.grid(row=i, column=0, sticky='ew')
+            token_label.grid(row=i, column=1, sticky='ew')
 
-            self.lexeme.grid(row=i, column=0, sticky='ew')
-            self.token.grid(row=i, column=1, sticky='ew')
+            # Bind callbacks for token highlighting
+            lexeme_label.bind("<Enter>", lambda ev, t=token: code_editor.format(Tags.TOKEN_HIGHLIGHT.name,
+                                                                                tuple(t.position),
+                                                                                tuple(t.end_position)))
+            token_label.bind("<Enter>", lambda ev, t=token: code_editor.format(Tags.TOKEN_HIGHLIGHT.name,
+                                                                               tuple(t.position),
+                                                                               tuple(t.end_position)))
+            lexeme_label.bind("<Leave>", lambda ev: code_editor.clear_format())
+            token_label.bind("<Leave>", lambda ev: code_editor.clear_format())
+
+            self.lexemes_labels.append(lexeme_label)
+            self.token_labels.append(token_label)
+
 
 class LexerCanvas(CTkCanvas):
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        CTkCanvas.__init__(self, master=master, width=16, borderwidth=0,highlightthickness=0, bg='#1A1B26')
+        CTkCanvas.__init__(self, master=master, width=16, borderwidth=0, highlightthickness=0, bg='#1A1B26')
 
-        self.grid_columnconfigure((0,1), weight=1)
-        self.grid_rowconfigure((0,1), weight=1)
+        self.grid_columnconfigure((0, 1), weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
 
         self.render_table()
 
