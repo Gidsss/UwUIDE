e40bc350bc7de15df1451a214a85eb7478594330
Naxus <jlprobantes2021@plm.edu.ph>
2023-11-05 10:19:23 +0800

    Change Import Icon

--- constants/path.py
+++ constants/path.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 RUN_ASSET = Path('./assets/run.png')
-LOAD_ASSET = Path('./assets/load.png')
+LOAD_ASSET = Path('./assets/import.png')
 SAVE_ASSET = Path('./assets/save.png')
 LOGO_WHITE_ASSET = Path('./assets/logo_white.png')
 LOGO_BLACK_ASSET = Path('./assets/logo_black.png')
