e93f5dbb1e57d4614b12db8679d8e9956134e90a
Gidsss <88811389+Gidsss@users.noreply.github.com>
2023-11-11 21:30:14 +0800

    Update token.py

--- src/lexer/token.py
+++ src/lexer/token.py
@@ -96,7 +96,7 @@ class TokenType(Enum):
     GEN_CWASS_NAME = ("CWASS_NAME", "cwass")
     SINGLE_LINE_COMMENT = ("COMMENT", "single_line_comment")
     MULTI_LINE_COMMENT = ("MULTI LINE COMMENT", "line")
-    TYPE_INDICATOR = ("TYPE INDICATOR", 'type_indicator')
+    
 
 
 class UniqueTokenType:
