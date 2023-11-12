3fd77e37a9435e299e31c55ced52c109a540017c
am-cid <mcidantonio@gmail.com>
2023-11-13 01:20:48 +0800

    move_cursor: remove self references

--- src/lexer/lexer_components/move_cursor.py
+++ src/lexer/lexer_components/move_cursor.py
@@ -1,54 +1,54 @@
-def _advance(self, increment: int = 1) -> bool:        
+def _advance(increment: int = 1) -> bool:        
     # initial check if EOF already
-    temp_position = self._position[1]
+    temp_position = position[1]
     temp_position += increment
-    end_of_file = temp_position > len(self._lines[len(self._lines)-1])-1 and self._position[0] == len(self._lines)-1
+    end_of_file = temp_position > len(lines[len(lines)-1])-1 and position[0] == len(lines)-1
     if end_of_file:
-        self._position[1] += increment
-        self._current_char = None
+        position[1] += increment
+        current_char = None
         return True
     
     # increment cursor
     # go to next line first char if out of bounds in current line (index 'length+1')
-    self._position[1] += increment
-    current_line_length = len(self._lines[self._position[0]])
-    while self._position[1] >= current_line_length:
-        self._position[1] -= current_line_length
-        self._position[0] += 1
-        current_line_length = len(self._lines[self._position[0]])
+    position[1] += increment
+    current_line_length = len(lines[position[0]])
+    while position[1] >= current_line_length:
+        position[1] -= current_line_length
+        position[0] += 1
+        current_line_length = len(lines[position[0]])
     
     # check if EOF after incrementing
     # if not EOF, read current character since we're sure we are not out of bounds otherwise due to previous check
-    end_of_file = self._position[0] >= len(self._lines)
+    end_of_file = position[0] >= len(lines)
     if end_of_file:
-        self._current_char = None
+        current_char = None
         return True
     else:
-        self._current_char = self._lines[self._position[0]][self._position[1]]
+        current_char = lines[position[0]][position[1]]
         return False
 
-def _reverse(self, increment: int = 1) -> bool:
+def _reverse(increment: int = 1) -> bool:
     # initial check if BOF already
-    temp_position = self._position[1]
+    temp_position = position[1]
     temp_position -= increment
-    beginning_of_file = temp_position < 0 and self._position[0] == 0
+    beginning_of_file = temp_position < 0 and position[0] == 0
     if beginning_of_file:
-        self._position[1] -= increment
-        self._current_char = None
+        position[1] -= increment
+        current_char = None
         return True
     
     # decrement cursor
     # go to previous line last char if out of bounds in current line (index '-1')
-    self._position[1] -= increment
-    while self._position[1] < 0:
-        self._position[0] -= 1
-        current_line_length = len(self._lines[self._position[0]])
-        self._position[1] += current_line_length
+    position[1] -= increment
+    while position[1] < 0:
+        position[0] -= 1
+        current_line_length = len(lines[position[0]])
+        position[1] += current_line_length
 
     # assign current char since we're sure we are not out of bounds
-    self._current_char = self._lines[self._position[0]][self._position[1]]
+    current_char = lines[position[0]][position[1]]
 
     # check if BOF after decrementing, return true if so
-    beginning_of_file = self._position == [0,0]
+    beginning_of_file = position == [0,0]
     return True if beginning_of_file else False
         
