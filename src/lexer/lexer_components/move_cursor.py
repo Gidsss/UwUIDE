594c65f2a1b1ea22151e9a5743c6f94b81164e50
am-cid <mcidantonio@gmail.com>
2023-11-13 01:27:48 +0800

    move_cursor: return curr_char bc string != mutable

--- src/lexer/lexer_components/move_cursor.py
+++ src/lexer/lexer_components/move_cursor.py
@@ -8,7 +8,7 @@ def _advance(context: tuple[list[str], list[int], str], increment: int = 1) -> b
     if end_of_file:
         position[1] += increment
         current_char = None
-        return True
+        return True, current_char
     
     # increment cursor
     # go to next line first char if out of bounds in current line (index 'length+1')
@@ -22,16 +22,13 @@ def _advance(context: tuple[list[str], list[int], str], increment: int = 1) -> b
     # check if EOF after incrementing
     # if not EOF, read current character since we're sure we are not out of bounds otherwise due to previous check
     end_of_file = position[0] >= len(lines)
-    if end_of_file:
-        current_char = None
-        return True
-    else:
-        current_char = lines[position[0]][position[1]]
-        return False
+
+    current_char = lines[position[0]][position[1]] if not end_of_file else None
+    return end_of_file, current_char
 
 def _reverse(context: tuple[list[str], list[int], str], increment: int = 1) -> bool:
     lines, position, current_char = context
-    
+
     # initial check if BOF already
     temp_position = position[1]
     temp_position -= increment
@@ -39,7 +36,7 @@ def _reverse(context: tuple[list[str], list[int], str], increment: int = 1) -> b
     if beginning_of_file:
         position[1] -= increment
         current_char = None
-        return True
+        return True, current_char
     
     # decrement cursor
     # go to previous line last char if out of bounds in current line (index '-1')
@@ -54,5 +51,6 @@ def _reverse(context: tuple[list[str], list[int], str], increment: int = 1) -> b
 
     # check if BOF after decrementing, return true if so
     beginning_of_file = position == [0,0]
-    return True if beginning_of_file else False
-        
+
+    current_char = lines[position[0]][position[1]] if not beginning_of_file else None
+    return beginning_of_file, current_char
