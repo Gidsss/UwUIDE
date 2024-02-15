d189fe816bbc6d0467199d2a6a27f48c7f8f1347
am-cid <mcidantonio@gmail.com>
2024-02-15 16:11:40 +0800

    print source code for easier debugging

--- src/parser/__main__.py
+++ src/parser/__main__.py
@@ -3,10 +3,29 @@ from ..lexer import Lexer
 
 if __name__ == "__main__":
     sc = """
-    fwunc aqua-chan() [[
+    fwunc shion-chan() [[
+
     ]]
+
+    fwunc shion-chan() [[
+
+    ]]
+
+    fwunc shion-chan() [[
+
+    ]]
+
     """
     source: list[str] = [line if line else '\n' for line in sc.split("\n")]
+    max_digit_length = len(str(len(source)))
+    max_width = max(len(line) for line in source) + max_digit_length + 3
+
+    print("-" * (max_width) )
+    for i, line in enumerate(source):
+        line = line if line != '\n' else ''
+        print(f"{i+1:<{max_digit_length}} | {line}")
+    print("-" * (max_width) )
+
     l = Lexer(source)
     if len(l.errors) > 0:
         print(l.print_error_logs())
