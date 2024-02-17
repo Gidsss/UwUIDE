2ba3992412faf285837023dfa72d82780c7194b5
am-cid <mcidantonio@gmail.com>
2024-02-17 23:24:37 +0800

    abstract code that interacts with peek module; use methods to keep body clean

--- src/parser/__main__.py
+++ src/parser/__main__.py
@@ -3,11 +3,11 @@ from ..lexer import Lexer, print_lex
 
 if __name__ == "__main__":
     sc = """
-    gwobaw shion-chan = 1+1~
-    gwobaw aqua-chan[] = {1+1, 2+-2, 3+3*3, 4/4-4}~
+    gwobaw aqua-chan[] = {1==1, 2-2!=fax, aqua>=shion+-ojou, 4/4%4}~
+    gwobaw shion-chan = 1+-aqua~
+    gwobaw ojou-chan =-5--1~
+    gwobaw sora-senpai = "tokino '| -nickname |' sora"~
     """
-    # gwobaw sora-senpai = "tokino '| -nickname |' sora"~
-    # gwobaw ojou-chan-dono = -5----1~
 
     source: list[str] = [line if line else '\n' for line in sc.split("\n")]
     max_digit_length = len(str(len(source)))
