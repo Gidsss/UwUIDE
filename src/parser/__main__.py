f8e05db482d9944c986a913af926784dd7e40a75
am-cid <mcidantonio@gmail.com>
2024-02-19 17:53:07 +0800

    if else test sample code

--- src/parser/__main__.py
+++ src/parser/__main__.py
@@ -3,27 +3,34 @@ from ..lexer import print_lex
 
 if __name__ == "__main__":
     sc = """
-    >//<
-        shape of this should be [3, 2, 1]
-        3 elements on aqua
-        2 elements in aqua[1]
-        1 elements in aqua[0][0]
-    >//<
-    gwobaw aqua-chan[2][-shion] = {
-        {
-            {
-                (2+2)*2,
-            }
-        }, 
-        {
-            3+3*3,
-            (4+4)*4,
-        },
-        1,
-    }~
-    gwobaw ojou-chan = shion(1, 2, aqua(3+4*5), lap("hello |-name--| world"))~
-    gwobaw shion-chan = 1+- (1-2) --*3-- --5 == 5~
+    iwf (1 == 2) [[
+        aqua-chan = 3~
+    ]] ewse iwf (4 == 5) [[
+        aqua-chan = 6~
+    ]] ewse [[
+        aqua-chan = 7~
+    ]]
     """
+    # >//<
+    # shape of this should be [3, 2, 1]
+    # 3 elements on aqua
+    # 2 elements in aqua[1]
+    # 1 elements in aqua[0][0]
+    # >//<
+    # gwobaw aqua-chan[2][-shion] = {
+    #     {
+    #         {
+    #             (2+2)*2,
+    #         }
+    #     }, 
+    #     {
+    #         3+3*3,
+    #         (4+4)*4,
+    #     },
+    #     1,
+    # }~
+    #     gwobaw ojou-chan = shion(1, 2, aqua(3+4*5), lap("hello |-name--| world"))~
+    #     gwobaw shion-chan = 1+- (1-2) -- == 5 -3-- *-5 ~
     # gwobaw sora-senpai = "tokino '| -nickname |' sora"~
     # gwobaw lap-chan[5]-dono = {1,2,3,4,5,6,7,8,9,10}~
     # gwobaw suba-chan[5] = {2+2, (3+3)*3}~
