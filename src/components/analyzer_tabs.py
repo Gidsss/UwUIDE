475266ccd05dde492188f3cbfefa33e1344334c8
chaaals <charlesching0808@gmail.com>
2024-02-26 19:21:04 +0800

    feat: integrated parser tree ui to parser package

--- src/components/analyzer_tabs.py
+++ src/components/analyzer_tabs.py
@@ -1,6 +1,8 @@
+import uuid
 from tkinter import ttk
 from customtkinter import *
 from .lexer_table import LexerCanvas
+from src.parser import Production, Program
 
 class UwUParserTab(CTkScrollableFrame):
     def __init__(self, master, **kwargs):
@@ -13,37 +15,63 @@ class UwUParserTab(CTkScrollableFrame):
         treestyles.configure('Treeview', background="#1A1B26", foreground="#FFFFFF", fieldbackground="#1A1B26", borderwidth=0)
         treestyles.map('Treeview', background=[('selected', "#1A1B26")], foreground=[('selected', "#89ca78")])
 
-        tree = ttk.Treeview(self, height=36, show='tree')
-        tree.insert('', 'end', text='Mainuwu', iid=0, open=False)
-        tree.insert('', 'end', text='Functions', iid=1, open=False)
-        tree.insert('', 'end', text='Globals', iid=2, open=False)
-        tree.insert('', 'end', text='Classes', iid=3, open=False)
+        self.tree = ttk.Treeview(self, height=100, show='tree')
+        self.tree_items = []
+        self.tree.insert('', 'end', text='Mainuwu', iid=0, open=False)
+        self.tree.insert('', 'end', text='Functions', iid=1, open=False)
+        self.tree.insert('', 'end', text='Globals', iid=2, open=False)
+        self.tree.insert('', 'end', text='Classes', iid=3, open=False)
 
-        tree.insert('0', 'end', iid=4, text='Function')
-        tree.insert('1', 'end', iid=5, text='Function')
-        tree.insert('2', 'end', iid=6, text='Declare')
-        tree.insert('3', 'end', iid=7, text='Class')
+        self.tree.grid(row=0, column=0, columnspan=2, sticky='nsew')
+    
+    def update_parser_tree(self, program: Program):
+        def traverse_tree(node, parent: str | Production | None = None):
+            """
+                Recursively loop the tree
+            """
+            if(node.child_nodes() == None):
+                self.tree.insert(parent, 'end', text=node.header())
+                return
+            
+            node_iid = uuid.uuid4()
+            self.tree_items.append(node_iid)
 
-        # mainuwu
-        tree.insert('4', 'end', 'mainuwu',text="mainuwu")
-        tree.insert('mainuwu', 'end', 'mainuwu-rtype',text="rtype")
-        tree.insert('mainuwu', 'end', 'mainuwu-paramters',text="parameters")
-        tree.insert('mainuwu', 'end', 'mainuwu-body',text="body")
+            self.tree.insert(parent, 'end', iid=node_iid, text=node.header())
+            try:
+                print(node.condition)
+            except:
+                pass
+            
+            for n in node.child_nodes():
+                if(not isinstance(n, Production)):
+                    if n:
+                        if(not isinstance(n, list)):
+                            self.tree.insert(node_iid, 'end', text=n)
+                        else:
+                            for p in n:
+                                traverse_tree(node=p, parent=node_iid)
+                else:
+                    traverse_tree(node=n, parent=node_iid)
 
-        # function
-        tree.insert('5', 'end', 'subfn1',text="testfn1")
-        tree.insert('subfn1', 'end', 'subfn1-rtype',text="rtype")
-        tree.insert('subfn1', 'end', 'subfn1-paramters',text="parameters")
-        tree.insert('subfn1', 'end', 'subfn1-body',text="body")
+        if(len(self.tree_items) > 0):
+            for item in self.tree_items:
+                try:
+                    self.tree.delete(item)
+                except:
+                    continue
+            
+            self.tree_items = []
 
-        tree.insert('','end', text="")
+        traverse_tree(node=program.mainuwu, parent='0')
+        
+        for fn in program.functions:
+            traverse_tree(node=fn, parent='1')
+        
+        for g in program.globals:
+            traverse_tree(node=g, parent='2')
 
-        tree.insert('5', 'end', 'subfn2',text="testfn2")
-        tree.insert('subfn2', 'end', 'subfn2-rtype',text="rtype")
-        tree.insert('subfn2', 'end', 'subfn2-paramters',text="parameters")
-        tree.insert('subfn2', 'end', 'subfn2-body',text="body")
-
-        tree.grid(row=0, column=0, columnspan=2, sticky='nsew')
+        for c in program.classes:
+            traverse_tree(node=c, parent='3')
 
 class UwULexerTab(CTkScrollableFrame):
     def __init__(self, master, **kwargs):
