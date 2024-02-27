12214c61dbeef1e1f98c12cc2436211763200adf
chaaals <charlesching0808@gmail.com>
2024-02-27 12:53:21 +0800

    refactor: modified loop tree algo

--- src/components/analyzer_tabs.py
+++ src/components/analyzer_tabs.py
@@ -25,33 +25,29 @@ class UwUParserTab(CTkScrollableFrame):
         self.tree.grid(row=0, column=0, columnspan=2, sticky='nsew')
     
     def update_parser_tree(self, program: Program):
-        def traverse_tree(node, parent: str | Production | None = None):
+        def loop_tree(node, parent: str |  None = None, key: str | None = None):
             """
                 Recursively loop the tree
             """
             if(node.child_nodes() == None):
-                self.tree.insert(parent, 'end', text=node.header())
+                self.tree.insert(parent, 'end', text=f"{key}: {node.header()}" if key else node.header())
                 return
             
             node_iid = uuid.uuid4()
             self.tree_items.append(node_iid)
 
             self.tree.insert(parent, 'end', iid=node_iid, text=node.header())
-            try:
-                print(node.condition)
-            except:
-                pass
-            
-            for n in node.child_nodes():
-                if(not isinstance(n, Production)):
-                    if n:
-                        if(not isinstance(n, list)):
-                            self.tree.insert(node_iid, 'end', text=n)
+
+            for k, v in node.child_nodes().items():
+                if(not isinstance(v, Production)):
+                    if v:
+                        if(not isinstance(v, list)):
+                            self.tree.insert(node_iid, 'end', text=f"{k}: {v}")
                         else:
-                            for p in n:
-                                traverse_tree(node=p, parent=node_iid)
+                            for p in v:
+                                loop_tree(node=p, parent=node_iid)
                 else:
-                    traverse_tree(node=n, parent=node_iid)
+                    loop_tree(node=v, parent=node_iid, key=k)
 
         if(len(self.tree_items) > 0):
             for item in self.tree_items:
@@ -62,16 +58,16 @@ class UwUParserTab(CTkScrollableFrame):
             
             self.tree_items = []
 
-        traverse_tree(node=program.mainuwu, parent='0')
+        loop_tree(node=program.mainuwu, parent='0')
         
         for fn in program.functions:
-            traverse_tree(node=fn, parent='1')
+            loop_tree(node=fn, parent='1')
         
         for g in program.globals:
-            traverse_tree(node=g, parent='2')
+            loop_tree(node=g, parent='2')
 
         for c in program.classes:
-            traverse_tree(node=c, parent='3')
+            loop_tree(node=c, parent='3')
 
 class UwULexerTab(CTkScrollableFrame):
     def __init__(self, master, **kwargs):
