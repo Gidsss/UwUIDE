60248794e63909af760297e062f38c1d22c4b3fa
am-cid <mcidantonio@gmail.com>
2024-03-15 01:01:33 +0800

    fix: make condition of while loop explicit

--- src/parser/__main__.py
+++ src/parser/__main__.py
@@ -5,7 +5,7 @@ from .error_handler import ErrorSrc
 if __name__ == "__main__":
     sc = """
     fwunc mainuwu-san() [[
-        a[2].a.a.a[1].a[3].b.a.b()~
+        a[2].a.a.a[1].a[3].b.a.b() = 2~
         a = a[2].a().a.a[1].a[3].b~
     ]]
     """
