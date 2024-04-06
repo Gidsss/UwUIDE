0425e811e306b21798d0db4025c5d8f159901ec2
am-cid <mcidantonio@gmail.com>
2024-04-06 22:35:50 +0800

    [analyzer] fix: make use of other analysis methods in analyzing array literals

--- src/analyzer/analyzer.py
+++ src/analyzer/analyzer.py
@@ -233,25 +233,25 @@ class MemberAnalyzer:
                 raise ValueError(f"Unknown collection: {collection}")
 
     def analyze_array_literal(self, array_literal: ArrayLiteral, local_defs: dict[str, tuple[Token, GlobalType]]) -> bool:
+        res = True
         for elem in array_literal.elements:
             match elem:
                 case Token():
-                    match elem.token:
-                        case UniqueTokenType():
-                            if not elem.string() in local_defs:
-                                self.errors.append(UndefinedError(
-                                    elem,
-                                ))
-                                return False
+                    if not (tmp := self.analyze_token(elem, local_defs)):
+                        res = tmp
                 case Expression():
-                    return self.analyze_expression(elem, local_defs)
+                    if not (tmp := self.analyze_expression(elem, local_defs)):
+                        res = tmp
                 case IdentifierProds():
-                    return self.analyze_ident_prods(elem, local_defs)
+                    if not (tmp := self.analyze_ident_prods(elem, local_defs)):
+                        res = tmp
                 case Collection():
-                    return self.analyze_collection(elem, local_defs)
+                    if not (tmp := self.analyze_collection(elem, local_defs)):
+                        res = tmp
                 case _:
                     raise ValueError(f"Unknown array element: {elem}")
-        return True
+        return res
+    
     def analyze_array_indices(self, index_list: list[Production], local_defs: dict[str, tuple[Token, GlobalType]]) -> bool:
         res = True
         for idx in index_list:
