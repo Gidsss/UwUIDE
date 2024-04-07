7308b0f8eeddf3387c6405fc793269b5d404aed5
am-cid <mcidantonio@gmail.com>
2024-04-07 21:04:10 +0800

    [builtins] feat: add helper fn to expect certain types only

--- src/builtin/types/types.py
+++ src/builtin/types/types.py
@@ -1,1 +1,8 @@
 
+class TypeError(Exception):
+    pass
+def expect_type_is_in(actual, expecteds: list[type], msg: str):
+    for expected in expecteds:
+        if not isinstance(actual, expected):
+            raise TypeError(f"{msg}\nExpected any in {expecteds} !!\nGot {type(actual)} !!!")
+    return True
