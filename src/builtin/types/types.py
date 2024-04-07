f9343dfe6ca2442a237e84c808bc21ca34d7ef8b
am-cid <mcidantonio@gmail.com>
2024-04-07 21:05:27 +0800

    [builtins] feat: add operator dunder methods for String and Array

--- src/builtin/types/types.py
+++ src/builtin/types/types.py
@@ -4,6 +4,28 @@ class String_UWU:
                                msg=f"OwO... that ain't stringable!")
         self.val: str = val
 
+    ## META DUNDER METHODS
+    # basic properties
+    def __len__(self):
+        return len(self.val)
+    def __str__(self):
+        return self.val
+
+    # operator overloading
+    def __add__(self, other):
+        'concatenates two strings'
+        expect_type_is_in(other, self.valid_operands(),
+                               msg=f"OwO... you can't add that!")
+        return String_UWU(self.val + str(other))
+    def __eq__(self, other):
+        expect_type_is_in(other, self.stringable_types(),
+                               msg=f"OwO... you can't compare with that!")
+        return self.val == other
+    def __ne__(self, other):
+        expect_type_is_in(other, self.stringable_types(),
+                               msg=f"OwO... you can't compare with that!")
+        return self.val != other
+
     ## UTILS
     def stringable_types(self) -> list[type]:
         return [str, String_UWU, int, float, bool, Array_UWU]
@@ -12,6 +34,24 @@ class String_UWU:
 class Array_UWU:
     def __init__(self, vals: list):
         self.val: list = vals
+
+    ## META DUNDER METHODS
+    # basic properties
+    def __len__(self):
+        return len(self.val)
+    def __str__(self):
+        return str(self.val)
+
+    # operator overloading
+    def __eq__(self, other):
+        expect_type_is_in(other, self.valid_operands(),
+                               msg=f"OwO... you can't compare with that!")
+        return self.val == other
+    def __ne__(self, other):
+        expect_type_is_in(other, self.valid_operands(),
+                               msg=f"OwO... you can't compare with that!")
+        return self.val != other
+
     ## UTILS
     def valid_array_elems(self) -> list[type]:
         return [str, String_UWU, int, float, bool, Array_UWU]
