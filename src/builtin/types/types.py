3ef2be5db135aaa937490f05510ca8afb9a7a59b
am-cid <mcidantonio@gmail.com>
2024-04-07 21:04:50 +0800

    [builtins] feat: String and Array init

--- src/builtin/types/types.py
+++ src/builtin/types/types.py
@@ -1,3 +1,22 @@
+class String_UWU:
+    def __init__(self, val: str):
+        expect_type_is_in(val, self.stringable_types(),
+                               msg=f"OwO... that ain't stringable!")
+        self.val: str = val
+
+    ## UTILS
+    def stringable_types(self) -> list[type]:
+        return [str, String_UWU, int, float, bool, Array_UWU]
+    def valid_operands(self) -> list[type]:
+        return [str, String_UWU]
+class Array_UWU:
+    def __init__(self, vals: list):
+        self.val: list = vals
+    ## UTILS
+    def valid_array_elems(self) -> list[type]:
+        return [str, String_UWU, int, float, bool, Array_UWU]
+    def valid_operands(self) -> list[type]:
+        return [list, Array_UWU]
 
 class TypeError(Exception):
     pass
