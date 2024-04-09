37f51e3dd653499ebe688effc18579c2fb376877
am-cid <mcidantonio@gmail.com>
2024-04-09 23:52:38 +0800

    [analyzer] feat: clearer error messages (new global type)

--- src/analyzer/error_handler.py
+++ src/analyzer/error_handler.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from src.lexer.token import Token
+from src.parser.productions import ClassAccessor, FnCall, IndexedIdentifier, Value
+from src.lexer.token import Token, TokenType
 from src.style import AnsiColor, Styled
 
 class GlobalType(Enum):
@@ -12,9 +13,9 @@ class GlobalType(Enum):
     IDENTIFIER = "IDENTIFIER"
     FUNCTION = "FUNCTION"
     CLASS = "CLASS"
-    LOCAL_DEF = "LOCAL_DEF"
     CLASS_PROPERTY = "CLASS_PROPERTY"
     CLASS_METHOD = "CLASS_METHOD"
+    LOCAL_CLASS_ID = "LOCAL_CLASS_ID"
 
 class ErrorSrc:
     src = [""]
@@ -30,7 +31,7 @@ class DuplicateDefinitionError:
         index_str = str(self.original.position[0] + 1)
         dupe_index = str(self.duplicate.position[0] + 1)
         max_pad = max(len(index_str), len(dupe_index))
-        border = f"\t{'_' * (len(ErrorSrc.src[self.original.position[0]]) + len(str(self.original.position[0] + 1)) + 3)}\n"
+        border = f"\t{'_' * (len(ErrorSrc.src[self.original.position[0]]) + len(str(self.original.position[0] + 1)) + max_pad)}\n"
         og_range = 1 if self.original.end_position is None else self.original.end_position[1] - self.original.position[1] + 1
         error_range = 1 if self.duplicate.end_position is None else self.duplicate.end_position[1] - self.duplicate.position[1] + 1
 
@@ -57,16 +58,17 @@ class DuplicateDefinitionError:
         return msg
 
 class UndefinedError:
-    def __init__(self, token: Token) -> None:
+    def __init__(self, token: Token, gtype: GlobalType) -> None:
         self.token = token
+        self.gtype = gtype
 
     def __str__(self):
         index_str = str(self.token.position[0] + 1)
         max_pad = len(index_str)
-        border = f"\t{'_' * (len(ErrorSrc.src[self.token.position[0]]) + len(str(self.token.position[0] + 1)) + 3)}\n"
+        border = f"\t{'_' * (len(ErrorSrc.src[self.token.position[0]]) + len(str(self.token.position[0] + 1)) + max_pad)}\n"
         error_range = 1 if self.token.end_position is None else self.token.end_position[1] - self.token.position[1] + 1
 
-        msg = f"Undefined: {self.token}\n"
+        msg = f"Undefined {self.gtype}: {self.token}\n"
         msg += border
         msg += f"\t{' ' * max_pad} | \t"
         msg += Styled.sprintln(
@@ -74,6 +76,5 @@ class UndefinedError:
             color=AnsiColor.RED)
         msg += f"\t{index_str:{max_pad}} | {ErrorSrc.src[self.token.position[0]]}\n"
         msg += f"\t{' ' * max_pad} | {' ' * self.token.position[1]}{'^' * (error_range)}\n"
-        msg += f"\t{' ' * max_pad} | {'_' * (self.token.position[1])}|\n"
         msg += border
         return msg
