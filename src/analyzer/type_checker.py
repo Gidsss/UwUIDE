0f270e41cce08da796f3d71bbbeed06db131dd0e
am-cid <mcidantonio@gmail.com>
2024-04-11 15:32:12 +0800

    [analyzer] fix: compare against UniqueTokenTypes strings
    same UniqueTokenType lexeme but different instance == not equal.
    chore: remove reference from analyzer

--- src/analyzer/type_checker.py
+++ src/analyzer/type_checker.py
@@ -117,8 +117,8 @@ class TypeChecker:
 
     def check_return(self, ret: ReturnStatement, return_type: Token, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
         actual_type = self.evaluate_value(ret.expr, local_defs)
-        if actual_type != return_type.token:
-            print(f"ERROR: {return_type} != {actual_type}")
+        if actual_type.string() != return_type.string():
+            print(f"ERROR: expected return type: {return_type}\n\tgot: {actual_type}")
 
     def check_declaration(self, decl: Declaration | ArrayDeclaration, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
         self.check_value(decl.value, decl.dtype, local_defs)
@@ -153,7 +153,9 @@ class TypeChecker:
                         for arg in value.args:
                             self.evaluate_value(arg, local_defs)
                     case _:
-                        print(f"ERROR: {expected_type} != {self.evaluate_value(value, local_defs)}")
+                        actual_type = self.evaluate_value(value, local_defs)
+                        if actual_type.string() != expected_type.string():
+                            print(f"ERROR: {expected_type} != {actual_type}")
 
     def evaluate_value(self, value: Value | Token, local_defs: dict[str, tuple[Token, GlobalType]]) -> TokenType:
         match value:
@@ -230,275 +232,13 @@ class TypeChecker:
         return [TokenType.EQUALITY_OPERATOR, TokenType.INEQUALITY_OPERATOR,
                 TokenType.AND_OPERATOR, TokenType.OR_OPERATOR]
 
-    ### REFERENCE FROM ANALYZER
-    # def analyze_class(self, cwass: Class) -> None:
-    #     local_defs: dict[str, tuple[Token, GlobalType]] = self.global_names.copy()
-    #     for p in cwass.params:
-    #         self.analyze_param(p, local_defs)
-    #     for prop in cwass.properties:
-    #         self.analyze_declaration(prop, local_defs)
-    #     for method in cwass.methods:
-    #         self.analyze_function(method)
-    #
-    # def analyze_param(self, param: Parameter, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     assert isinstance(param.id, Token)
-    #     if param.id.string() in local_defs:
-    #         self.errors.append(DuplicateDefinitionError(
-    #             *local_defs[param.id.string()],
-    #             param.id,
-    #             GlobalType.IDENTIFIER,
-    #         ))
-    #     else:
-    #         local_defs[param.id.string()] = (param.id, GlobalType.IDENTIFIER)
-    #
-    # def analyze_args(self, args: list[Value], local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     for arg in args:
-    #         self.analyze_value(arg, local_defs)
-    #
-    # def analyze_body(self, body: BlockStatement, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     when you're calling analyze_body(), make sure to pass in a copy of local_defs
-    #     '''
-    #     for stmt in body.statements:
-    #         match stmt:
-    #             case Print():
-    #                 self.analyze_print(stmt, local_defs)
-    #             case Input():
-    #                 self.analyze_input(stmt, local_defs)
-    #             case Declaration() | ArrayDeclaration():
-    #                 self.analyze_declaration(stmt, local_defs)
-    #             case Assignment():
-    #                 self.analyze_assignment(stmt, local_defs)
-    #             case IfStatement():
-    #                 self.analyze_if(stmt, local_defs)
-    #             case WhileLoop():
-    #                 self.analyze_while_loop(stmt, local_defs)
-    #             case ForLoop():
-    #                 self.analyze_for_loop(stmt, local_defs.copy())
-    #             case ReturnStatement():
-    #                 self.analyze_return(stmt, local_defs)
-    #             case FnCall():
-    #                 self.analyze_ident_prods(stmt, local_defs)
-    #             case _:
-    #                 raise ValueError(f"Unknown statement: {stmt}")
-    #
-    # ## IN BODY ANALYZERS
-    # def analyze_print(self, print_stmt: Print, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     for val in print_stmt.values:
-    #         self.analyze_value(val, local_defs)
-    #
-    # def analyze_input(self, input_stmt: Input, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     self.analyze_value(input_stmt.expr, local_defs)
-    #
-    # def analyze_if(self, if_stmt: IfStatement | ElseIfStatement, local_defs: dict[str, tuple[Token, GlobalType]], else_if: bool = False) -> None:
-    #     self.analyze_value(if_stmt.condition, local_defs)
-    #     self.analyze_body(if_stmt.then, local_defs.copy())
-    #     if else_if: return
-    #     assert isinstance(if_stmt, IfStatement)
-    #     for elif_stmt in if_stmt.else_if:
-    #         self.analyze_if(elif_stmt, local_defs, else_if=True)
-    #     if if_stmt.else_block:
-    #         self.analyze_body(if_stmt.else_block, local_defs.copy())
-    #
-    # def analyze_declaration(self, decl: Declaration | ArrayDeclaration, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     self.expect_unique_token(decl.id, local_defs)
-    #     self.analyze_value(decl.value, local_defs)
-    #
-    # def analyze_assignment(self, assign: Assignment, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     match assign.id:
-    #         case Token():
-    #             self.expect_defined_token(assign.id, local_defs)
-    #         case IdentifierProds():
-    #             self.analyze_ident_prods(assign.id, local_defs)
-    #         case _:
-    #             raise ValueError(f"Unknown assignment left hand side: {assign.id}")
-    #     self.analyze_value(assign.value, local_defs)
-    #
-    # def analyze_while_loop(self, while_loop: WhileLoop, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     self.analyze_value(while_loop.condition, local_defs)
-    #     self.analyze_body(while_loop.body, local_defs.copy())
-    #
-    # def analyze_for_loop(self, for_loop: ForLoop, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     when calling analyze_for_loop(), make sure to pass in a copy of local_defs
-    #     '''
-    #     match for_loop.init:
-    #         case Assignment():
-    #             self.analyze_assignment(for_loop.init, local_defs)
-    #         case Declaration() | ArrayDeclaration():
-    #             self.analyze_declaration(for_loop.init, local_defs)
-    #         case Token():
-    #             self.expect_defined_token(for_loop.init, local_defs)
-    #         case IdentifierProds():
-    #             self.analyze_ident_prods(for_loop.init, local_defs)
-    #         case _:
-    #             raise ValueError(f"Unknown for loop init: {for_loop.init}")
-    #     self.analyze_value(for_loop.condition, local_defs)
-    #     self.analyze_value(for_loop.update, local_defs)
-    #     self.analyze_body(for_loop.body, local_defs.copy())
-    #
-    # def analyze_return(self, ret: ReturnStatement, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     self.analyze_value(ret.expr, local_defs)
-    #
-    # ## OTHER ANALYZERS
-    # def analyze_value(self, value: Value | Token, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     match value:
-    #         case Token():
-    #             self.expect_defined_token(value, local_defs)
-    #         case Expression():
-    #             self.analyze_expression(value, local_defs)
-    #         case IdentifierProds():
-    #             self.analyze_ident_prods(value, local_defs)
-    #         case Iterable():
-    #             self.analyze_iterable(value, local_defs)
-    #         case _:
-    #             raise ValueError(f"Unknown value: {value}")
-    # def analyze_ident_prods(self, ident_prod: IdentifierProds, local_defs: dict[str, tuple[Token, GlobalType]],
-    #                         access_depth: int = 1) -> None:
-    #     match ident_prod:
-    #         case IndexedIdentifier():
-    #             match ident_prod.id:
-    #                 case Token():
-    #                     self.expect_defined_token(ident_prod.id, local_defs)
-    #                 case FnCall():
-    #                     self.analyze_fn_call(ident_prod.id, local_defs)
-    #             self.analyze_array_indices(ident_prod.index, local_defs)
-    #         case FnCall():
-    #             self.analyze_fn_call(ident_prod, local_defs)
-    #         case ClassConstructor():
-    #             self.analyze_args(ident_prod.args, local_defs)
-    #         case ClassAccessor():
-    #             if access_depth > 0:
-    #                 match ident_prod.id:
-    #                     case Token():
-    #                         self.expect_defined_token(ident_prod.id, local_defs)
-    #                     case FnCall():
-    #                         self.analyze_fn_call(ident_prod.id, local_defs)
-    #             match ident_prod.accessed:
-    #                 case FnCall():
-    #                     self.analyze_fn_call(ident_prod.accessed, local_defs)
-    #                 case IndexedIdentifier():
-    #                     self.analyze_array_indices(ident_prod.accessed.index, local_defs)
-    #                 case ClassAccessor():
-    #                     self.analyze_ident_prods(ident_prod.accessed, local_defs, access_depth=access_depth+1)
-    #         case _:
-    #             raise ValueError(f"Unknown identifier production: {ident_prod}")
-    #
-    # def analyze_fn_call(self, fn_call: FnCall, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     if fn_call.id.string() in local_defs and local_defs[fn_call.id.string()][1] == GlobalType.FUNCTION:
-    #         pass
-    #     else:
-    #         self.errors.append(UndefinedError(
-    #             fn_call.id,
-    #             GlobalType.FUNCTION,
-    #         ))
-    #     self.analyze_args(fn_call.args, local_defs)
-    #
-    # def analyze_iterable(self, collection: Iterable, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     this is for array literals, string fmt, and string literals
-    #     try not to use analyze_array_literal, analyze_string_fmt, or analyze_string_literal directly.
-    #     use this method instead.
-    #     '''
-    #     match collection:
-    #         case ArrayLiteral():
-    #             self.analyze_array_literal(collection, local_defs)
-    #         case StringFmt():
-    #             self.analyze_string_fmt(collection, local_defs)
-    #         case Input():
-    #             self.analyze_input(collection, local_defs)
-    #         case StringLiteral():
-    #             self.analyze_string_literal(collection, local_defs)
-    #         case _:
-    #             raise ValueError(f"Unknown collection: {collection}")
-    #
-    # def analyze_string_literal(self, str_lit: StringLiteral, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     try not to use this method directly.
-    #     use analyze_iterable instead
-    #     '''
-    #     for concat in str_lit.concats:
-    #         match concat:
-    #             case StringFmt():
-    #                 self.analyze_string_fmt(concat, local_defs)
-    #             case Input():
-    #                 self.analyze_input(concat, local_defs)
-    #             case StringLiteral():
-    #                 self.analyze_string_literal(concat, local_defs)
-    #
-    # def analyze_array_literal(self, array_literal: ArrayLiteral, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     try not to use this method directly.
-    #     use analyze_iterable instead
-    #     '''
-    #     for elem in array_literal.elements:
-    #         self.analyze_value(elem, local_defs)
-    # 
-    # def analyze_array_indices(self, index_list: list[Value], local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     for idx in index_list:
-    #         self.analyze_value(idx, local_defs)
-    # 
-    # def analyze_expression(self, expr: Expression, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     match expr:
-    #         case PrefixExpression():
-    #             self.analyze_value(expr.right, local_defs)
-    #         case PostfixExpression():
-    #             self.analyze_value(expr.left, local_defs)
-    #         case InfixExpression():
-    #             self.analyze_value(expr.left, local_defs)
-    #             self.analyze_value(expr.right, local_defs)
-    #         case _:
-    #             raise ValueError(f"Unknown expression: {expr}")
-    #
-    # def analyze_string_fmt(self, string_fmt: StringFmt, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     try not to use this method directly.
-    #     use analyze_iterable instead
-    #     '''
-    #     for expr in string_fmt.exprs:
-    #         self.analyze_value(expr, local_defs)
-    #
-    #     for concat in string_fmt.concats:
-    #         match concat:
-    #             case StringFmt() | Input() | StringLiteral():
-    #                 self.analyze_iterable(concat, local_defs)
-    #             case _:
-    #                 raise ValueError(f"Unknown concat: {concat}")
-    #
-    #
-    # ## HELPER METHODS
-    # def expect_defined_token(self, token: Token, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     checks whether the token is defined in the scope
-    #     if not, appends to errors
-    #     '''
-    #     match token.token:
-    #         case TokenType.STRING_LITERAL | TokenType.INT_LITERAL | TokenType.FLOAT_LITERAL | TokenType.FAX | TokenType.CAP | TokenType.NUWW:
-    #             pass
-    #         case UniqueTokenType():
-    #             if token.string() in local_defs:
-    #                 pass
-    #             else:
-    #                 self.errors.append(UndefinedError(
-    #                     token,
-    #                     GlobalType.IDENTIFIER,
-    #                 ))
-    #         case _:
-    #             raise ValueError(f"Unknown token: {token}")
-    # def expect_unique_token(self, token: Token, local_defs: dict[str, tuple[Token, GlobalType]]) -> None:
-    #     '''
-    #     checks whether the token is not yet defined in the scope
-    #     if already defined (duplicate), appends to errors
-    #     '''
-    #     match token.token:
-    #         case UniqueTokenType():
-    #             if token.string() in local_defs and local_defs[token.string()][1] == GlobalType.IDENTIFIER:
-    #                 self.errors.append(DuplicateDefinitionError(
-    #                     *local_defs[token.string()],
-    #                     token,
-    #                     GlobalType.IDENTIFIER,
-    #                 ))
-    #             else:
-    #                 local_defs[token.string()] = (token, GlobalType.IDENTIFIER)
-    #         case _:
-    #             raise ValueError(f"Unknown token: {token}")
+    def flatten_array(self, arr: list[Value]) -> list[Value]:
+        res = []
+        for item in arr:
+            if isinstance(item, list):
+                res += self.flatten_array(item)
+            elif isinstance(item, ArrayLiteral):
+                res += self.flatten_array(item.elements)
+            else:
+                res.append(item)
+        return res
