3ad4ddfa0508dc048c3979a315ce433963b130f8
am-cid <mcidantonio@gmail.com>
2024-04-20 15:44:53 +0800

    [parser] feat: add a placeholder dtype for `Assignment`s
    This is to be assigned during type checking so conversion to python string will ensure the correct type

--- src/parser/productions.py
+++ src/parser/productions.py
@@ -385,6 +385,7 @@ class Assignment(Statement):
     def __init__(self):
         self.id: Token | IndexedIdentifier | ClassAccessor = Token() 
         self.value: Value = Value()
+        self.dtype: Token = Token()
 
     def header(self):
         return f"assign: {self.id.header()}"
