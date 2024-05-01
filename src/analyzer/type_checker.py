771a809961a2f3b9295780ff7fa08e0b3767eea6
am-cid <mcidantonio@gmail.com>
2024-05-02 00:12:13 +0800

    [analyzer] refactor: fn call args use match instead of else if

--- src/analyzer/type_checker.py
+++ src/analyzer/type_checker.py
@@ -746,12 +746,13 @@ class TypeChecker:
                 case _:
                     actual = self.evaluate_value(arg, local_defs)
             actual_types.append(actual)
-            if expected.token == TokenType.ARRAY_ELEMENT:
-                matches.append(self.is_element_of_expected(actual, self_type if self_type.exists() else expected.token, arg))
-            elif expected.token == TokenType.GEN_ARRAY:
-                matches.append(self.is_similar_type(actual.flat_string(), self_type.flat_string() if self_type.exists() else expected.flat_string(), arg))
-            else:
-                matches.append(self.is_similar_type(actual.flat_string(), expected.flat_string(), arg, is_call=True))
+            match expected.token:
+                case TokenType.ARRAY_ELEMENT:
+                    matches.append(self.is_element_of_expected(actual, self_type if self_type.exists() else expected.token, arg))
+                case TokenType.GEN_ARRAY:
+                    matches.append(self.is_similar_type(actual.flat_string(), self_type.flat_string() if self_type.exists() else expected.flat_string(), arg))
+                case _:
+                    matches.append(self.is_similar_type(actual.flat_string(), expected.flat_string(), arg, is_call=True))
 
         if not all(matches) or len(call_args) != len(expected_types):
             expected_types_str = []
