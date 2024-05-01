d1e1f5741e9d3c338f45a0f878d8e94fba064ccf
am-cid <mcidantonio@gmail.com>
2024-05-01 22:22:05 +0800

    [builtins] feat: ensure val is capped even after operations

--- src/builtin/types/number.py
+++ src/builtin/types/number.py
@@ -1,11 +1,8 @@
 class Int:
     def __init__(self, val):
         try:
-            self.val: int = int(float(val))
-            if self.val > 9999999999:
-                self.val = 9999999999
-            elif self.val < -9999999999:
-                self.val = -9999999999
+            res = int(float(val))
+            self.val: int = self.cap_val(res)
         except:
             raise ValueError(f"Oh no!! '{val}' cannot be converted to chaaaaaaaaannnnnnnnnn!!")
 
@@ -22,25 +19,25 @@ class Int:
             res = int(float(other))
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to chaaaaaaaaannnnnnnnnn!!")
-        return type(self)(self.val + res)
+        return type(self)(self.cap_val(self.val + res))
     def __sub__(self, other) -> "Int":
         try:
             res = int(float(other))
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to chaaaaaaaaannnnnnnnnn!!")
-        return type(self)(self.val - res)
+        return type(self)(self.cap_val(self.val - res))
     def __mul__(self, other) -> "Int":
         try:
             res = int(float(other))
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to chaaaaaaaaannnnnnnnnn!!")
-        return type(self)(self.val * res)
+        return type(self)(self.cap_val(self.val * res))
     def __truediv__(self, other) -> "Int":
         try:
             res = int(float(other))
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to chaaaaaaaaannnnnnnnnn!!")
-        return type(self)(self.val / res)
+        return type(self)(self.cap_val(self.val / res))
     def __neg__(self) -> "Int":
         return type(self)(-self.val)
     def __lt__(self, other) -> bool:
@@ -96,16 +93,21 @@ class Int:
 
     ## BUILTIN METHODS
     def _abs(self) -> "Int":
-        return type(self)(abs(self.val))
+        return type(self)(self.cap_val(abs(self.val)))
+
+    ## HELPER METHODS
+    def cap_val(self, val) -> int:
+        if val > 9999999999:
+            val = 9999999999
+        elif val < -9999999999:
+            val = -9999999999
+        return val
 
 class Float:
     def __init__(self, val):
         try:
-            self.val: float = float(val)
-            if self.val > 9999999999.0:
-                self.val = 9999999999.0
-            elif self.val < -9999999999.0:
-                self.val = -9999999999.0
+            res = float(val)
+            self.val: float = self.cap_val(res)
         except:
             raise ValueError(f"Oh no!! '{val}' cannot be converted to kuuuuuuuunnnnnnn!!")
 
@@ -122,25 +124,25 @@ class Float:
             res = float(other)
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to kuuuuuuuunnnnnnn!!")
-        return type(self)(self.val + res)
+        return type(self)(self.cap_val(self.val + res))
     def __sub__(self, other) -> "Float":
         try:
             res = float(other)
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to kuuuuuuuunnnnnnn!!")
-        return type(self)(self.val - res)
+        return type(self)(self.cap_val(self.val - res))
     def __mul__(self, other) -> "Float":
         try:
             res = float(other)
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to kuuuuuuuunnnnnnn!!")
-        return type(self)(self.val * res)
+        return type(self)(self.cap_val(self.val * res))
     def __truediv__(self, other) -> "Float":
         try:
             res = float(other)
         except:
             raise ValueError(f"Oh no!! '{other}' cannot be converted to kuuuuuuuunnnnnnn!!")
-        return type(self)(self.val / res)
+        return type(self)(self.cap_val(self.val / res))
     def __neg__(self) -> "Float":
         return type(self)(-self.val)
     def __lt__(self, other) -> bool:
@@ -196,4 +198,12 @@ class Float:
 
     ## BUILTIN METHODS
     def _abs(self) -> "Float":
-        return type(self)(abs(self.val))
+        return type(self)(self.cap_val(abs(self.val)))
+
+    ## HELPER METHODS
+    def cap_val(self, val) -> float:
+        if val > 9999999999.0:
+            val = 9999999999.0
+        elif val < -9999999999.0:
+            val = -9999999999.0
+        return val
