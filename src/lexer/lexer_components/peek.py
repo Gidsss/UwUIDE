fb98058bfea6f6f518253c656d2992c2b3665df0
am-cid <mcidantonio@gmail.com>
2024-05-20 14:33:14 +0800

    [lexer] fix: String tokens ranges are not accurate

--- src/lexer/lexer_components/peek.py
+++ src/lexer/lexer_components/peek.py
@@ -361,7 +361,7 @@ def string(context: tuple[list[str], list[int], str, list[Token], list[DelimErro
                 _, current_char = reverse_cursor(context)
                 context = lines, position, current_char, tokens, logs
 
-            starting_position = (position[0], position[1]-len(temp_string.replace('\\\\', '\\'))+1)
+            starting_position = (position[0], position[1]-len(temp_string) + 1 + temp_string.count('|'))
             ending_position = (position[0], position[1])
             logs.append(GenericError(Error.UNCLOSED_STRING, starting_position, ending_position,
                                             context = f"'{temp_string}' is unclosed"))
@@ -387,7 +387,7 @@ def string(context: tuple[list[str], list[int], str, list[Token], list[DelimErro
 
             temp_string += current_char
             temp_string = '"' + temp_string[1:-1] + '"'
-            temp_string_length = len(temp_string.replace('\\\\', '\\'))
+            temp_string_length = len(temp_string) + temp_string.count('|')
 
             starting_position = (position[0], position[1]-temp_string_length+1)
             ending_position = (position[0], position[1])
