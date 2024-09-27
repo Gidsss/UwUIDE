5aba8313c4eee17b0289a986a330a1f0372ed4bf
am-cid <mcidantonio@gmail.com>
2024-09-28 00:12:16 +0800

    [analyzer, compiler, lexer, parser] feat: implement source from files/package-<package>.uwu

--- src/compiler/__main__.py
+++ src/compiler/__main__.py
@@ -1,3 +1,4 @@
+from constants.path import COMPILE_SOURCE
 from src.lexer import Lexer
 from src.parser import Parser
 from src.parser.error_handler import ErrorSrc as parErrorSrc
@@ -6,33 +7,18 @@ from src.analyzer.error_handler import ErrorSrc
 from .compiler import Compiler
 
 if __name__ == "__main__":
-    sc = """
-    fwunc mainuwu-san() [[
-        num-chan = inpwt("Enter number to be squared: ")~
-        times-chan = inpwt("Enter number of times to square: ")~
-        iwf (num > 0) [[
-            fow(i-chan = 0~ i < times~ i + 1) [[
-                pwint(num)~
-                num = square(num)~
-            ]]
-        ]] ewse [[
-            pwint("Enter a positive number. '| num |' is not a positive number.")~
-        ]]
-    ]]
-    fwunc square-chan(num-chan) [[
-        wetuwn(num*num)~
-    ]]
-    """
-    source: list[str] = [line if line else '\n' for line in sc.split("\n")]
+    source: list[str] = [
+        line if line else "\n" for line in open(COMPILE_SOURCE, "r").readlines()
+    ]
     max_digit_length = len(str(len(source)))
     max_width = max(len(line) for line in source) + max_digit_length + 3
-    print('\nsample text file')
-    print("-"*max_width)
+    print("\nsample text file")
+    print("-" * max_width)
     for i, line in enumerate(source):
-        line = line if line != '\n' else ''
-        print(f"{i+1} | {line}")
-    print("-"*max_width)
-    print('end of file\n')
+        line = line if line != "\n" else ""
+        print(f"{i + 1} | {line}")
+    print("-" * max_width)
+    print("end of file\n")
 
     l = Lexer(source)
     if l.errors:
